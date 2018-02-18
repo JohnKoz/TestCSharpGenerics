8d78bd5f7ac9eca204949fdbca061b684f19b69d
John Kozlowski <John.Kozlowski@gmail.com>
2018-02-18 15:09:04 -0600

    Initial commit as well as generics 1&2 sections

---
+++ 2-csharp-generics-m2-collections-exercise-files/excercises/after/CollectIt/CollectIt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeesByName = new Dictionary<string, List<Employee>>();
+
+            employeesByName.Add("John", new List<Employee>() { new Employee { Name = "John" } });
+
+            employeesByName["John"].Add(new Employee() { Name = "John" });
+            
+            foreach (var item in employeesByName)
+            {
+                foreach (var employee in item.Value)
+                {
+                    Console.WriteLine(employee.Name);
+                }
+            }
+
+        }
+    }
+}
